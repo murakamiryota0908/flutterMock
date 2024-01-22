2bc8bcc787838daf5b2eb6e206196a23b2a72a8f
murakamiryota0908 <murakamiryota0908@gmail.com>
2024-01-22 20:07:43 +0900

    darkmode

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -6,44 +6,40 @@ import 'memopage.dart';
 class homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        body: Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(top: 32),
-              child: Text('アルバイト管理アプリ'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                OutlinedButton(
-                  child: Text('シフト管理'),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => shiftpage()));
-                  },
-                ),
-                OutlinedButton(
-                  child: Text('給与管理'),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => salarypage()));
-                  },
-                ),
-                OutlinedButton(
-                  child: Text('仕事のメモ'),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => memopage()));
-                  },
-                ),
-              ],
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Work Management Application"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              OutlinedButton(
+                child: Text('シフト管理'),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => shiftpage()));
+                },
+              ),
+              OutlinedButton(
+                child: Text('給与管理'),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => salarypage()));
+                },
+              ),
+              OutlinedButton(
+                child: Text('仕事のメモ'),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => memopage()));
+                },
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
